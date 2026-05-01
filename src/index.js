00619fb2425bad367e045d42c9984e2315a9655f
russkih.kristina2 <russkih.kristina2@rwb.ru>
2026-05-01 13:47:10 +0300

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const closeByOpen = new Map(bracketsConfig);
+  const opens = new Set(bracketsConfig.map(([open]) => open));
+  const stack = [];
+
+  const isValid = str.split('').every((char) => {
+    const last = stack[stack.length - 1];
+    const closesLast = closeByOpen.get(last) === char;
+    if (closesLast) {
+      stack.pop();
+      return true;
+    }
+
+    const isOpen = opens.has(char);
+    if (isOpen) {
+      stack.push(char);
+      return true;
+    }
+
+    return false;
+  });
+
+  return isValid && !stack.length;
 };
